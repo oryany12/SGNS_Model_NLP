333580513cdd256068573516376350544ef92e07
Oryan Yehezkel <oryany12@gmail.com>
2023-05-08 14:40:03 +0300

    Initial commit

---
+++ ex2_api.py
@@ -0,0 +1,157 @@
+"""
+API for ex2, implementing the skip-gram model (with negative sampling).
+
+"""
+
+# you can use these packages (uncomment as needed)
+import pickle
+#import pandas as pd
+#import numpy as np
+#import os,time, re, sys, random, math, collections, nltk
+
+
+#static functions
+def who_am_i():  # this is not a class method
+    """Returns a dictionary with your name, id number and email. keys=['name', 'id','email']
+        Make sure you return your own info!
+    """
+    return {'name': 'John Doe', 'id': '012345678', 'email': 'jdoe@post.bgu.ac.il'}
+
+
+def normalize_text(fn):
+    """ Loading a text file and normalizing it, returning a list of sentences.
+
+    Args:
+        fn: full path to the text file to process
+    """
+    sentences = []
+
+    #TODO
+
+    return sentences
+
+def sigmoid(x): return 1.0 / (1 + np.exp(-x))
+
+
+def load_model(fn):
+    """ Loads a model pickle and return it.
+
+    Args:
+        fn: the full path to the model to load.
+    """
+
+    #TODO
+    return sg_model
+
+
+class SkipGram:
+    def __init__(self, sentences, d=100, neg_samples=4, context=4, word_count_threshold=5):
+        self.sentences = sentences
+        self.d = d  # embedding dimension
+        self.neg_samples = neg_samples  # num of negative samples for one positive sample
+        self.context = context #the size of the context window (not counting the target word)
+        self.word_count_threshold = word_count_threshold #ignore low frequency words (appearing under the threshold)
+
+        # Tips:
+        # 1. It is recommended to create a word:count dictionary
+        # 2. It is recommended to create a word-index map
+
+        # TODO
+
+    def compute_similarity(self, w1, w2):
+        """ Returns the cosine similarity (in [0,1]) between the specified words.
+
+        Args:
+            w1: a word
+            w2: a word
+        Retunrns: a float in [0,1]; defaults to 0.0 if one of specified words is OOV.
+    """
+        sim  = 0.0 # default
+        #TODO
+
+        return sim # default
+
+    def get_closest_words(self, w, n=5):
+        """Returns a list containing the n words that are the closest to the specified word.
+
+        Args:
+            w: the word to find close words to.
+            n: the number of words to return. Defaults to 5.
+        """
+
+
+    def learn_embeddings(self, step_size=0.001, epochs=50, early_stopping=3, model_path=None):
+        """Returns a trained embedding models and saves it in the specified path
+
+        Args:
+            step_size: step size for  the gradient descent. Defaults to 0.0001
+            epochs: number or training epochs. Defaults to 50
+            early_stopping: stop training if the Loss was not improved for this number of epochs
+            model_path: full path (including file name) to save the model pickle at.
+        """
+
+
+        vocab_size = ... #todo: set to be the number of words in the model (how? how many, indeed?)
+        T = np.random.rand(self.d, vocab_size) # embedding matrix of target words
+        C = np.random.rand(vocab_size, self.d)  # embedding matrix of context words
+
+        #tips:
+        # 1. have a flag that allows printing to standard output so you can follow timing, loss change etc.
+        # 2. print progress indicators every N (hundreds? thousands? an epoch?) samples
+        # 3. save a temp model after every epoch
+        # 4.1 before you start - have the training examples ready - both positive and negative samples
+        # 4.2. it is recommended to train on word indices and not the strings themselves.
+
+        # TODO
+
+        return T,C
+
+    def combine_vectors(self, T, C, combo=0, model_path=None):
+        """Returns a single embedding matrix and saves it to the specified path
+
+        Args:
+            T: The learned targets (T) embeddings (as returned from learn_embeddings())
+            C: The learned contexts (C) embeddings (as returned from learn_embeddings())
+            combo: indicates how wo combine the T and C embeddings (int)
+                   0: use only the T embeddings (default)
+                   1: use only the C embeddings
+                   2: return a pointwise average of C and T
+                   3: return the sum of C and T
+                   4: concat C and T vectors (effectively doubling the dimention of the embedding space)
+            model_path: full path (including file name) to save the model pickle at.
+        """
+
+        # TODO
+
+        return V
+
+    def find_analogy(self, w1,w2,w3):
+        """Returns a word (string) that matches the analogy test given the three specified words.
+           Required analogy: w1 to w2 is like ____ to w3.
+
+        Args:
+             w1: first word in the analogy (string)
+             w2: second word in the analogy (string)
+             w3: third word in the analogy (string)
+        """
+
+        #TODO
+
+        return w
+
+    def test_analogy(self, w1, w2, w3, w4, n=1):
+        """Returns True if sim(w1-w2+w3, w4)@n; Otherwise return False.
+            That is, returning True if w4 is one of the n closest words to the vector w1-w2+w3.
+            Interpretation: 'w1 to w2 is like w4 to w3'
+
+        Args:
+             w1: first word in the analogy (string)
+             w2: second word in the analogy (string)
+             w3: third word in the analogy (string)
+             w4: forth word in the analogy (string)
+             n: the distance (work rank) to be accepted as similarity
+            """
+
+        # TODO
+
+        return False
